781e9ae005a790d67e3e5b2d1e72887a0e9860cf
Rathijit Paul <alienpapon009@gmail.com>
2020-11-08 18:57:33 +0600

    Inbox View Added

--- src/views/Inbox/index.js
+++ src/views/Inbox/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {NavLink} from "react-router-dom";
 
 import LayoutWrapper from "../../layouts/LayoutWrapper";
 
@@ -7,34 +8,59 @@ import style from "./styles";
 const profileData = require("../../assets/profileData.json");
 
 const Inbox = (props) => {
-    const {mainContainer, nameDivContainer, personContainer, profileIconContainer, textContainer, profileNameContainer, messageContainer} = style();
+    const {mainContainer, nameDivContainer, profileImageContainer,profileNameContainer, messageBoxContainer, inputContainer, sendButtonContainer, messageDivContainer, messageSubContainer, messageContainer} = style();
     
-    const inboxId = props.match.params.id;
+    // const inboxId = props.match.params.id;
+    const message = profileData.messages[0];
+    const [messageText, setMessageText] = useState("");
+
+    const handleInputChange = (e) => {
+        if(e.target.name === "message") {
+            const value = e.target.value;
+
+            value ? setMessageText(value) : setMessageText("");
+        }
+    };
+
+    const onSendMessage = () => {
+        console.log(messageText);
+        setMessageText("");
+    };
 
     return ( 
         <LayoutWrapper>
             
-                <div className={mainContainer}>
+            <div className={mainContainer}>
+
+                <div className={nameDivContainer}>
+                    <NavLink exact to="/myprofile">
+                        <img src={message.profileImage} alt="" className={profileImageContainer} />
+                    </NavLink>
                     
-                    <div className={nameDivContainer}>
-                        {profileData.messages.map(message => (
-                            <div className={personContainer} key={message._id}>
-                                <img src={message.profileImage} alt="" className={profileIconContainer} />
+                    <NavLink exact to="/myprofile" className={profileNameContainer}>{message.fullName}</NavLink>
+                </div>
 
-                                <div className={textContainer}>
-                                    <div className={profileNameContainer}>
-                                        {message.fullName + " " + inboxId}
-                                    </div>
-
-                                    <div className={messageContainer}>
-                                        {message.message[0].received ? "" : "You: "}  {message.message[0].text}
-                                    </div>
-                                </div>
-                            </div>
+                <div className={messageDivContainer}>
+                    <div className={messageSubContainer}>
+                        {message.message.map(message => (
+                            <div key={message._id} className={messageContainer} style={message.received === "true" ? {textAlign: "left"} : {textAlign: "right"}}>{message.text}</div>
                         ))}
                     </div>
-                    
                 </div>
+
+                <div className={messageBoxContainer}>
+                    <textarea 
+                        type="text" 
+                        id="message"  
+                        name="message"  
+                        placeholder="Type your message"
+                        value={messageText}
+                        onChange={handleInputChange}
+                        className={inputContainer} />
+                    <div className={sendButtonContainer} onClick={onSendMessage}>Send</div>
+                </div>
+                    
+            </div>
         </LayoutWrapper>
      );
 }
--- src/views/Inbox/styles.js
+++ src/views/Inbox/styles.js
@@ -3,153 +3,191 @@ import { makeStyles } from "@material-ui/core/styles";
 const style = makeStyles((theme) => ({
   mainContainer: {
     display: "flex",
-    justifyContent: "space-between",
-
-    marginLeft: "25%",
-    marginRight: "25%",
+    justifyContent: "flex-start",
+    flexDirection: "column",
+    marginLeft: "15%",
+    marginRight: "15%",
 
     [theme.breakpoints.down(700)]: {
-      marginLeft: "10%",
-      marginRight: "10%",
+      marginLeft: "8%",
+      marginRight: "8%",
     },
 
     [theme.breakpoints.down(500)]: {
-      marginLeft: "5%",
-      marginRight: "5%",
+      marginLeft: "3%",
+      marginRight: "3%",
     },
   },
 
   nameDivContainer: {
       display: "flex",
       justifyContent: "flex-start",
-      flexDirection: "column",
-      width: "100%"
+      flexDirection: "row",
+      width: "70%",
+      marginBottom: "20px",
+      padding: "1%",
+      position: "absolute",
+      height: "220px",
+
+      [theme.breakpoints.down(700)]: {
+        width: "84%",
+      },
+  
+      [theme.breakpoints.down(500)]: {
+        width: "94%",
+      },
   },
 
-  personContainer: {
-    display: "flex",
-    justifyContent: "flex-start",
-    flexDirection: "row",
-    height: "50px",
-    padding: "10px",
-    overflow: "hidden",
-    
-    marginBottom: "5px",
-    cursor: "pointer",
-    backgroundColor: "#C9AE99",
-    borderRadius: "20px",
-
-    "&:hover": {
-        opacity: "0.3",
-    }
-  },
-
-  profileIconContainer: {
-    width: "50px",
-    height: "50px",
+  profileImageContainer: {
+    width: "100px",
+    height: "100px",
     borderRadius: "50%",
-    marginRight: "10px",
-  },
-
-  textContainer: {
-    display: "flex",
-    justifyContent: "flex-start",
-    flexDirection: "column",
-    marginTop: "3px",
+    marginRight: "30px",
 
     [theme.breakpoints.down(400)]: {
-      marginTop: "8px",
-  },
+      width: "80px",
+      height: "80px",
+    },
+
+    "&:hover": {
+      opacity: "0.4",
+    },
   },
 
   profileNameContainer: {
     fontFamily: "Lato",
-    fontSize: "18px",
+    fontSize: "25px",
     fontWeight: "bold",
     color: "#562727",
-    
-    whiteSpace: "nowrap",
+    // whiteSpace: "nowrap",
+    marginTop: "30px",
+    textAlign: "center",
+    textDecoration: "none",
 
     [theme.breakpoints.down(400)]: {
-        fontSize: "15px",
+      marginTop: "25px",
+        fontSize: "18px",
+    },
+
+    "&:hover": {
+      opacity: "0.4",
     },
   },
 
-  messageContainer: {
+  messageBoxContainer: {
+    display: "flex",
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    marginTop: "20px",
+    padding: "1%",
+    width: "70%",
+    position: "absolute",
+    bottom: "0",
+    height: "100px",
+
+    [theme.breakpoints.down(700)]: {
+      width: "84%",
+    },
+
+    [theme.breakpoints.down(500)]: {
+      width: "94%",
+    },
+  },
+
+  inputContainer: {
+    width: "65%",
+    height: "40%",
+    borderRadius: "20px",
+    fontFamily: "Ubuntu Mono",
+    fontSize: "18px",
+    padding: "2%",
+    resize: "none",
+    overflowY: "auto",
+    marginRight: "10px",
+  },
+
+  sendButtonContainer: {
+    cursor: "pointer",
+    width: "23%",
+    height: "30%",
+    marginTop: "10px",
+    marginBottom: "10px",
+    borderRadius: "20px",
+    backgroundColor: "#300eb7",
+    color: "#dbcebc",
     fontFamily: "Lato",
-    fontSize: "15px",
+    fontSize: "25px",
     fontWeight: "bold",
-    color: "#c637ac",
-    marginTop: "5px",
-    
-    whiteSpace: "nowrap",
+    textAlign: "center",
+    padding: "1.5%",
+
+    '&:hover': {
+      opacity: "0.35",
+    },
 
     [theme.breakpoints.down(400)]: {
-        fontSize: "12px",
+      fontSize: "15px",
+      paddingTop: "8px",
+      width: "21%",
+    },
+
+    [theme.breakpoints.down(300)]: {
+      marginTop: "12px",
+      paddingTop: "6px",
+      width: "18%",
     },
   },
 
   messageDivContainer: {
     display: "flex",
-    justifyContent: "flex-start",
+    justifyContent: "flex-end",
     flexDirection: "column",
-    width: "75%",
-    marginLeft: "10px",
-    marginRight: "10px",
-    borderRadius: "20px",
-    backgroundColor: "black",
-  },
+    width: "69%",
+    padding: "1%",
+    position: "absolute",
+    top: "220px",
+    bottom: "120px",
 
-  iconContainer: {
-    height: "25%",
-    width: "20%",
-    marginLeft:  "40%",
+    [theme.breakpoints.down(700)]: {
+      width: "83%",
+    },
 
     [theme.breakpoints.down(500)]: {
-      height: "15%",
-      width: "60%",
-      marginLeft:  "20%",
+      width: "93%",
+    },
+
+    [theme.breakpoints.down(400)]: {
+      top: "200px",
     },
   },
 
-  titleContainer: {
-    fontFamily: "Ubuntu Mono",
-    fontSize: "45px",
-    fontWeight: "bold",
-    color: "#502f84",
-    marginTop: "20px",
-    textAlign: "center",
+  messageSubContainer: {
+    // minHeight: "calc(100vh - 400px)",
+    backgroundColor: "#C9AE99",
+    borderRadius: "10px",
+    overflowY: "scroll",
+    scrollbarWidth: "none",
+    msOverflowStyle: "none",
+    padding: "10px",
 
-    [theme.breakpoints.down(500)]: {
-      fontSize: "35px",
-      marginTop: "10%",
-      marginBottom: "10%",
+    webkit_scrollbar: {
+      width: "0px",
+      background: "transparent",
     },
   },
 
-  descriptionContainer: {
-    fontFamily: "Ubuntu Mono",
-    fontSize: "17px",
+  messageContainer: {
+    fontFamily: "Lato",
+    fontSize: "20px",
     fontWeight: "bold",
-    fontStyle: "italic",
-    color: "#050a0c",
-    textAlign: "center",
-  },
+    color: "#c637ac",
+    marginBottom: "10px",
+    
+    whiteSpace: "nowrap",
 
-  navlinkContainer: {
-    fontFamily: "Ubuntu Mono",
-    fontSize: "30px",
-    fontWeight: "bold",
-    color: "#3d4adb",
-    cursor: "pointer",
-    textDecoration: "none",
-    marginTop: "30px",
-    textAlign: "center",
-
-    '&:hover': {
-      opacity: "0.4",
-      textDecoration: "underline",
-    }
+    // [theme.breakpoints.down(400)]: {
+    //     fontSize: "15px",
+    // },
   },
 }));
 
