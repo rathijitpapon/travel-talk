e79160ba4e227d45666188bee33b5f24eb164b06
Rathijit Paul <alienpapon009@gmail.com>
2020-11-24 22:01:50 +0600

    Add Message Service

--- src/views/Inbox/index.js
+++ src/views/Inbox/index.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import io from "socket.io-client";
 import {NavLink} from "react-router-dom";
+import { toast } from 'react-toastify';
+
+import userService from "../../services/userService";
+import authService from "../../services/authService";
 
 import LayoutWrapper from "../../layouts/LayoutWrapper";
 
 import style from "./styles";
 
-const profileData = require("../../assets/profileData.json");
+let socket;
 
 const Inbox = (props) => {
     const {mainContainer, nameDivContainer, profileImageContainer,profileNameContainer, messageBoxContainer, inputContainer, sendButtonContainer, messageDivContainer, messageSubContainer, messageContainer} = style();
-    
-    // const inboxId = props.match.params.id;
-    const message = profileData.messages[0];
+
+    const [message, setMessage] = useState({
+        message: [],
+    });
     const [messageText, setMessageText] = useState("");
+    const [profile, setProfile] = useState(null);
+    const [myProfile, setMyProfile] = useState(null);
+    const inboxId = props.match.params.id;
+    const messageScroll = useRef(null);
 
     const handleInputChange = (e) => {
         if(e.target.name === "message") {
@@ -23,44 +33,114 @@ const Inbox = (props) => {
     };
 
     const onSendMessage = () => {
-        console.log(messageText);
+        socket.emit("sendText", {
+            token: authService.getJWT() ? authService.getJWT() : "",
+            username: props.match.params.id,
+            messageText: messageText,
+        }, (error) => {
+            if(error) {
+                toast.error(error, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
+        });
         setMessageText("");
     };
 
+    useEffect(() => {
+        async function fetchProfile() {
+            const data = await userService.getProfile(inboxId);        
+            if(data.status < 400) {
+                setProfile(data.user);
+            }
+
+            const mydata = await userService.getProfile("my");        
+            if(mydata.status < 400) {
+                setMyProfile(mydata.user);
+            }
+
+            if(data.status >= 400) {
+                window.location = "/404";
+            }
+        }
+
+        fetchProfile();
+        
+        const endPoint = process.env.REACT_APP_API_BASE_URL;
+        socket = io(endPoint);
+
+        socket.emit("joinInbox", {
+            token: authService.getJWT() ? authService.getJWT() : "",
+            username: props.match.params.id,
+        }, (error) => {
+            if(error) {
+                toast.error("No Message Found", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
+        });
+    }, [inboxId, props.match.params.id]);
+
+    useEffect(() => {
+        socket.on("getInbox", (data) => {
+            setMessage(data.inbox);
+        });
+
+        if (messageScroll && messageScroll.current && messageScroll.current.addEventListener && message.message.length > 0) {
+            messageScroll.current.addEventListener('DOMNodeInserted', event => {
+                const { currentTarget: target } = event;
+                target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
+            });
+        }
+    });
+
     return ( 
         <LayoutWrapper>
-            
-            <div className={mainContainer}>
+            {(profile && myProfile) ? (
+                <div className={mainContainer}>
 
-                <div className={nameDivContainer}>
-                    <NavLink exact to="/myprofile">
-                        <img src={message.profileImage} alt="" className={profileImageContainer} />
-                    </NavLink>
-                    
-                    <NavLink exact to="/myprofile" className={profileNameContainer}>{message.fullName}</NavLink>
-                </div>
-
-                <div className={messageDivContainer}>
-                    <div className={messageSubContainer}>
-                        {message.message.map(message => (
-                            <div key={message._id} className={messageContainer} style={message.received === "true" ? {textAlign: "left"} : {textAlign: "right"}}>{message.text}</div>
-                        ))}
+                    <div className={nameDivContainer}>
+                        <NavLink exact to={"/profile/" + profile.username}>
+                            <img src={profile.profileImage} alt="" className={profileImageContainer} />
+                        </NavLink>
+                        
+                        <NavLink exact to={"/profile/" + profile.username} className={profileNameContainer}>{profile.fullname}</NavLink>
                     </div>
-                </div>
 
-                <div className={messageBoxContainer}>
-                    <textarea 
-                        type="text" 
-                        id="message"  
-                        name="message"  
-                        placeholder="Type your message"
-                        value={messageText}
-                        onChange={handleInputChange}
-                        className={inputContainer} />
-                    <div className={sendButtonContainer} onClick={onSendMessage}>Send</div>
+                    <div className={messageDivContainer}>
+                        <div className={messageSubContainer} ref={messageScroll}>
+                            {message.message.map(message => (
+                                <div key={message._id} className={messageContainer} style={message.sender.username === profile.username ? {textAlign: "left"} : {textAlign: "right"}}>{message.messageText}</div>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className={messageBoxContainer}>
+                        <textarea 
+                            type="text" 
+                            id="message"  
+                            name="message"  
+                            placeholder="Type your message"
+                            value={messageText}
+                            onChange={handleInputChange}
+                            className={inputContainer} />
+                        <div className={sendButtonContainer} onClick={onSendMessage}>Send</div>
+                    </div>
+                        
                 </div>
-                    
-            </div>
+            ) : null}
         </LayoutWrapper>
      );
 }
--- src/views/Messages/index.js
+++ src/views/Messages/index.js
@@ -1,39 +1,94 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import io from "socket.io-client";
 import {NavLink} from "react-router-dom";
+import { toast } from 'react-toastify';
+
+import authService from "../../services/authService";
+import userService from "../../services/userService";
 
 import LayoutWrapper from "../../layouts/LayoutWrapper";
 
 import style from "./styles";
 
-const profileData = require("../../assets/profileData.json");
+let socket;
 
-const Messages = () => {
+const Messages = (props) => {
     const {mainContainer, nameDivContainer, personContainer, profileIconContainer, textContainer, profileNameContainer, messageContainer} = style();
+
+    const [messages, setMessages] = useState(null);
+    const [profile, setProfile] = useState(null);
+
+    useEffect(() => {
+        async function fetchProfile() {
+            const data = await userService.getProfile("my");        
+            if(data.status < 400) {
+                setProfile(data.user);
+            }
+        }
+
+        fetchProfile();
+
+        const endPoint = process.env.REACT_APP_API_BASE_URL;
+        socket = io(endPoint);
+
+        socket.emit("joinMessages", {
+            token: authService.getJWT() ? authService.getJWT() : "",
+        }, (error) => {
+            if(error) {
+                toast.error(error, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
+        });
+
+    }, [props.match.params.id]);
+
+    useEffect(() => {
+        socket.on("getMessages", (data) => {
+            setMessages(data.messages);
+        });
+    });
 
     return ( 
         <LayoutWrapper>
-            
+            {(messages && profile) ? (
                 <div className={mainContainer}>
                     
                     <div className={nameDivContainer}>
-                        {profileData.messages.map(message => (
-                            <NavLink exact to={"/inbox/" + message._id} className={personContainer} key={message._id}>
-                                <img src={message.profileImage} alt="" className={profileIconContainer} />
+                        {messages.map(message => {
+                            let user = message.user1;
+                            const lastMessage = message.message[message.message.length - 1];
+                            if(profile.username === message.user1.username){
+                                user = message.user2
+                            }
+                            
+                            return (
+                                <NavLink exact to={"/inbox/" + user.username} className={personContainer} key={message._id}>
+                                    <img src={user.profileImage} alt="" className={profileIconContainer} />
 
-                                <div className={textContainer}>
-                                    <div className={profileNameContainer}>
-                                        {message.fullName}
-                                    </div>
+                                    <div className={textContainer}>
+                                        <div className={profileNameContainer}>
+                                            {user.fullname}
+                                        </div>
 
-                                    <div className={messageContainer}>
-                                        {message.message[0].received ? "" : "You: "}  {message.message[0].text}
+                                        <div className={messageContainer}>
+                                            {lastMessage.sender.username !== profile.username ? "" : "You: "}  {lastMessage.messageText}
+                                        </div>
                                     </div>
-                                </div>
-                            </NavLink>
-                        ))}
+                                </NavLink>
+                            )
+                        }
+                        )}
                     </div>
                     
                 </div>
+            ) : null}
         </LayoutWrapper>
      );
 }
